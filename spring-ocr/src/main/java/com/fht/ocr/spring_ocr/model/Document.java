b09d2672ac6abe0fab459ce706998bfc74b8d7f1
chellakudc97 <cchellakudam@hotmail.com>
2024-09-30 22:24:20 +0200

    *) added web ui for sprint 2

--- spring-ocr/src/main/java/com/fht/ocr/spring_ocr/model/Document.java
+++ spring-ocr/src/main/java/com/fht/ocr/spring_ocr/model/Document.java
@@ -1,4 +1,12 @@
 package com.fht.ocr.spring_ocr.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Document {
+    private String content;
 }
